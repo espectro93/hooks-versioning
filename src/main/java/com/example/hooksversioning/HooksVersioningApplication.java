3b0d17bce12bdd574573f4f876d0464360a8c1cb
steffen <gerdes.steffen@googlemail.com>
2022-08-27 09:55:27 +0200

    chore: Initial setup

---
+++ src/main/java/com/example/hooksversioning/HooksVersioningApplication.java
@@ -0,0 +1,13 @@
+package com.example.hooksversioning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HooksVersioningApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HooksVersioningApplication.class, args);
+    }
+
+}
